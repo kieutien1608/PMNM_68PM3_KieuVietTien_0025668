f459128a1fa1d2c59111eb6683a12133faee5086
kieutien1608 <tienkieu1608@gmail.com>
2026-05-15 12:30:33 +0700

    Khởi tạo project

---
+++ app/controllers/SinhvienController.php
@@ -0,0 +1,10 @@
+<?php
+class sinhvienController{
+    public function index(){
+        echo "Đây là trang tạo sinh viên";
+    }
+    public function create(){
+        echo "Đây là trang tạo sinh viên";
+    }
+}
+?>
---
+++ app/core/App.php
@@ -0,0 +1,37 @@
+<?php
+class App
+{
+    protected $controller = "home";
+    protected $action = "index";
+    protected $params = [];
+    public function __construct()
+    {
+        // if(isset($_GET['url'])){
+        //     echo ($_GET['url']);
+        // }
+        $urlProcess = $this->urlProcess();
+        if(isset($urlProcess[0])){
+            if(file_exists('../app/controllers/'.$urlProcess[0].'.php')){
+                $this->controller= $urlProcess[0];
+                unset($urlProcess[0]);
+            }
+        }
+        require_once '../app/controller/'.$this->controller.'.php';
+        $this->controller = new $this->controller;
+        if(isset($urlProcess[1])){
+            if(method_exists($this->controller,$urlProcess[1])){
+                $this->action = $urlProcess[1];
+                unset($urlProcess[1]);
+            }
+        }
+    $this->params = $urlProcess ? array_values($urlProcess) : [];
+    call_user_func_array([$this->controller, $this->action], $this->params);
+    }
+        
+    public function UrlProcess(){
+        if(isset($_GET['url'])){
+            return explode('/', filter_var(trim($_GET['url'], '/')));
+        }
+    }
+}
+?>  
