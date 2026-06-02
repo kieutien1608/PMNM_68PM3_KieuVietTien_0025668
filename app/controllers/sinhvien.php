f9e8b74f51962f44d20317f669ca32495c8600aa
kieutien1608 <tienkieu1608@gmail.com>
2026-06-02 22:16:39 +0700

    feat<Sinhvien>: create Sinhvien

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,14 +1,37 @@
 <?php
 require_once '../app/core/Controller.php';
-class sinhvien extends Controller {
-    function index() {
-        $SinhvienModel = $this->model('SinhvienModel');
-        $sinhvien = $SinhvienModel -> getAllSinhvien();
-        $this -> view('sinhvien/index', ['sinhvien' => $sinhvien]);
-    }
+class sinhvien extends Controller
+{
+  public function index()
+  {
+    $sinhvienModel = $this->model('sinhvienModel');
+    $sinhviens = $sinhvienModel->getAllSinhVien();
+    //trả về view
+    //require_once '../app/views/sinhvien/index.php';
+    $this->view("layout/masterlayout", ['viewname' => 'sinhvien/index', 'sinhviens' => $sinhviens, 'title' => 'Danh sách sinh viên']);
+  }
 
-    function create() {
-        require_once '../app/views/sinhvien/create.php';
+  public function create()
+  {
+    require_once '../app/views/sinhvien/create.php';
+  }
+  public function store()
+  {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $data = [
+        'hoten' => $_POST['hoten'],
+        'gioitinh' => $_POST['gioitinh'],
+        'mssv' => $_POST['mssv']
+      ];
+      $sinhvienModel = $this->model('sinhvienModel');
+      $result = $sinhvienModel->create($data);
+      if ($result) {
+        $_SESSION['success'] = "Thêm sinh viên thành công!";
+        header("Location: /sinhvien/index");
+        exit();
+      } else {
+        $_SESSION['error'] = "Thêm sinh viên thất bại!";
+      }
     }
+  }
 }
-?>
--- app/models/SinhvienModel.php
+++ app/models/SinhvienModel.php
@@ -1,16 +1,28 @@
 <?php
 require_once '../app/core/DB.php';
-class SinhvienModel {
+class sinhvienModel
+{
     private $conn;
     public function __construct()
     {
-        $this->conn = ConnectDB::Connect();
+        $this->conn = connectDB::Connect();
     }
-    public function getAllSinhvien(){
+    public function getAllSinhVien()
+    {
         $query = "SELECT * FROM tbl_sinhviens";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function create($data)
+    {
+        $query = "INSERT INTO tbl_sinhviens (hoten, gioitinh, mssv) VALUES (:hoten, :gioitinh, :mssv)";
+        $stmt = $this->conn->prepare($query);
+        if ($stmt->execute($data)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 ?>
