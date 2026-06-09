ed2774abbb612d53e2ad19174deb59f0d1caa22f
kieutien1608 <tienkieu1608@gmail.com>
2026-06-09 23:03:42 +0700

    feat<sinhvien>: paging

--- app/models/SinhvienModel.php
+++ app/models/SinhvienModel.php
@@ -1,28 +1,50 @@
 <?php
-require_once '../app/core/DB.php';
+require_once "../app/core/DB.php";
 class sinhvienModel
 {
-    private $conn;
-    public function __construct()
-    {
-        $this->conn = connectDB::Connect();
+  private $conn;
+  public function __construct()
+  {
+    $this->conn = ConnectDB::Connect();
+  }
+
+  public function getAllSinhVien()
+  {
+    $query = "SELECT * FROM sinhvien";
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function create($MSSV, $HoTen, $GioiTinh)
+  {
+    $query = "INSERT INTO sinhvien (MSSV, HoTen, GioiTinh) VALUES ( :MSSV, :HoTen, :GioiTinh )";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(':MSSV', $MSSV);
+    $stmt->bindParam(':HoTen', $HoTen);
+    $stmt->bindParam(':GioiTinh', $GioiTinh);
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      return false;
     }
-    public function getAllSinhVien()
-    {
-        $query = "SELECT * FROM tbl_sinhviens";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    public function create($data)
-    {
-        $query = "INSERT INTO tbl_sinhviens (hoten, gioitinh, mssv) VALUES (:hoten, :gioitinh, :mssv)";
-        $stmt = $this->conn->prepare($query);
-        if ($stmt->execute($data)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+  }
+
+  public function paging($limit = 5, $offset = 0, $search = "")
+  {
+    $query = "SELECT * FROM sinhvien LIMIT :limit OFFSET :offset";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+    $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Tính tổng số bảng ghi
+    $selectAllQuery = $this->conn->query("SELECT COUNT(*) FROM sinhvien");
+    $totalRecords = $selectAllQuery->fetchColumn();
+
+    $totalPages = ceil($totalRecords / $limit);
+
+    return ['sinhviens' => $result, 'totalPages' => $totalPages];
+  }
 }
-?>
--- app/views/sinhvien/index.php
+++ app/views/sinhvien/index.php
@@ -1,39 +1,66 @@
-<style>
-  table {
-    border-collapse: collapse;
-    width: 100%;
-  }
+<!DOCTYPE html>
+<html lang="en">
 
-  th,
-  td {
-    text-align: left;
-    padding: 8px;
-  }
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  tr:nth-child(even) {
-    background-color: #f2f2f2;
-  }
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
 
-  th {
-    background-color: #04AA6D;
-    color: white;
-  }
-</style>
+    th,
+    td {
+      text-align: left;
+      padding: 8px;
+    }
 
-<h1>Danh sách sinh viên</h1>
-<table>
-  <tr>
-    <th>ID</th>
-    <th>MSSV</th>
-    <th>Họ Tên</th>
-    <th>Giới Tính</th>
-  </tr>
-  <?php foreach ($sinhviens as $sinhvien) : ?>
+    tr:nth-child(even) {
+      background-color: #f2f2f2
+    }
+
+    th {
+      background-color: #04AA6D;
+      color: white;
+    }
+  </style>
+
+</head>
+
+<body>
+  <table>
     <tr>
-      <td><?php echo $sinhvien['id']; ?></td>
-      <td><?php echo $sinhvien['MSSV']; ?></td>
-      <td><?php echo $sinhvien['HoTen']; ?></td>
-      <td><?php echo $sinhvien['GioiTinh']; ?></td>
+      <th>STT</th>
+      <th>MSSV</th>
+      <th>Họ Tên</th>
+      <th>Giới Tính</th>
+      <th>Thao tác</th>
     </tr>
-  <?php endforeach; ?>
-</table>
+    <?php foreach ($sinhviens as $index => $sinhvien) : ?>
+      <tr>
+        <td><?php echo $index + 1; ?></td>
+        <td><?php echo $sinhvien['MSSV']; ?></td>
+        <td><?php echo $sinhvien['HoTen']; ?></td>
+        <td><?php echo $sinhvien['GioiTinh']; ?></td>
+        <td>
+          <a href="/sinhvien/edit/<?php echo $sinhvien['id']; ?>" class="btn btn-primary">Sửa</a>
+          <a href="/sinhvien/delete/<?php echo $sinhvien['id']; ?>" class="btn btn-danger" onclick="return confirm('Bạn có chắc chắn muốn xóa sinh viên này không?')">Xóa</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+  <div>
+    <?php
+    $pageSize = 5;
+    for ($i = 1; $i <= $totalPages; $i++) {
+      $offset = ($i - 1) * $pageSize;
+      echo "<a href='/sinhvien/index/$pageSize/$offset' class='btn btn-success' style='margin-right: 5px; margin-top: 5px;'>$i</a>";
+    }
+    ?>
+  </div>
+
+</body>
+
+</html>
