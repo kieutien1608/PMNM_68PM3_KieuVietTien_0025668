5f384df0a06b2b572a88f7e250412af65105ff4e
kieutien1608 <tienkieu1608@gmail.com>
2026-06-19 22:19:38 +0700

    feat<sinhvien>: sort by 'mssv', 'hoten'

--- app/core/DB.php
+++ app/core/DB.php
@@ -1,21 +1,31 @@
 <?php
-class ConnectDB{
-    private static $host = 'localhost';
-    private static $db_name = '68pm34';
-    private static $username = 'root';
-    private static $password = '';
-    public static $conn;
+ class connectDB
+ {
+     private static $host = "localhost";
+     private static $username = "root";
+     private static $password = "";
+     private static $dbname = "68pm34";
+     private static $conn;
 
-    public static function Connect(){
-        $conn = null;
-        try{
-            $conn = new PDO('mysql:host=' . self::$host . ';dbname=' . self::$db_name, self::$username, self::$password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
-            echo 'Connection Error: ' . $e->getMessage();
-        }
-        return $conn;
-    }
-}
-?>
-        
+     public static function Connect()
+     {
+         if (!self::$conn) {
+            try {
+               self::$conn = new PDO("mysql:host=" . self::$host . ":3307;dbname=" . self::$dbname, self::$username, self::$password);
+               self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $e) {
+               echo "loi ket noi: " . $e->getMessage();
+               die();
+            }
+         }
+         return self::$conn;
+     }
+
+     public function __destruct()
+     {
+         if (self::$conn) {
+             self::$conn = null;
+         }
+     }
+ }
